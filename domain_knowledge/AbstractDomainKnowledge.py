ac535607829c6e0354824f007b8d1f55c4840f55
hptruong93 <hptruong93@gmail.com>
2017-02-15 10:37:11 -0500

    Added temporary abstract data set class

--- domain_knowledge/AbstractDomainKnowledge.py
+++ domain_knowledge/AbstractDomainKnowledge.py
@@ -11,3 +11,32 @@ class AbstractDomainKnowledge(object):
     def __init__(self):
         super(AbstractDomainKnowledge, self).__init__()
 
+        self.data = None # Data used to train/validate/test the model. See class AbstractDataSet below
+
+
+class AbstractDataSet(object):
+    """
+        Abstract class for a data set. This class should be able to offer various operations for a data set so that we can easily
+        train/validate/test a model.
+    """
+
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self):
+        super(AbstractDataSet, self).__init__()
+
+    @abc.abstractmethod
+    def load_data(self):
+        pass
+
+    @abc.abstractmethod
+    def get_training_data(self):
+        pass
+
+    @abc.abstractmethod
+    def get_validation_data(self):
+        pass
+
+    @abc.abstractmethod
+    def get_testing_data(self):
+        pass
